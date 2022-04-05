966bb418521fc157526bd7d65a00dddeb6ec7e2a
Angela <ifeoluwaolubo@gmail.com>
2022-04-05 09:52:55 +0100

    Started dockerizing the microservice project

--- posts/index.js
+++ posts/index.js
@@ -1,36 +1,40 @@
-const express = require("express")
-const cors = require("cors")
-const axios = require("axios")
-const {randomBytes} = require("crypto")
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const { randomBytes } = require("crypto");
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const posts = {}
+const posts = {};
 
 app.get("/posts", (req, res) => {
-    res.status(200).json(posts)
-})
+  res.status(200).json(posts);
+});
 
 app.post("/posts", async (req, res) => {
-    const id = randomBytes(4).toString("hex") 
-    const {title} = req.body
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    posts[id] = {
-        id, title
-    }
+  posts[id] = {
+    id,
+    title,
+  };
 
-    await axios.post("http://localhost:8080/events", {type: "PostCreated", data: {id, title}})
+  await axios.post("http://localhost:8080/events", {
+    type: "PostCreated",
+    data: { id, title },
+  });
 
-    return res.status(201).json(posts[id])
-})
+  return res.status(201).json(posts[id]);
+});
 
 app.post("/events", (req, res) => {
-    console.log("Received Event", req.body.type)
-    return res.status(200).json({status: "OK"})
-})
+  console.log("Received Event", req.body.type);
+  return res.status(200).json({ status: "OK" });
+});
 
 app.listen(3030, () => {
-    console.log("Posts service started on port 3030")
-})
+  console.log("Posts service started on port 3030");
+});
--- query/index.js
+++ query/index.js
@@ -1,58 +1,57 @@
-const express = require("express")
-const cors = require("cors")
-const axios = require("axios")
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-const posts = {}
+const posts = {};
 
 const handleEvents = (type, data) => {
-    if (type === "PostCreated") {
-        const {id, title} = data
-        posts[id] = {id, title, comments: []}
-    }
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
 
-    if (type === "CommentCreated") {
-        const {id, content, postID, status} = data
-        const post = posts[postID]
-        post.comments.push({id, content, status})
-    }
+  if (type === "CommentCreated") {
+    const { id, content, postID, status } = data;
+    const post = posts[postID];
+    post.comments.push({ id, content, status });
+  }
 
-    if (type === "CommentUpdated") {
-        const { id, postID, status, content } = data
+  if (type === "CommentUpdated") {
+    const { id, postID, status, content } = data;
 
-        const post = posts[postID]
-        const comment = post.comments.find(comment => {
-            return comment.id == id
-        })
+    const post = posts[postID];
+    const comment = post.comments.find((comment) => {
+      return comment.id == id;
+    });
 
-        comment.status = status
-        comment.content = content
-    }
-}
+    comment.status = status;
+    comment.content = content;
+  }
+};
 
 app.get("/posts", (req, res) => {
-    return res.status(200).json(posts)
-})
+  return res.status(200).json(posts);
+});
 
 app.post("/events", (req, res) => {
-    const { type, data } = req.body
+  const { type, data } = req.body;
 
-    handleEvents(type, data)
+  handleEvents(type, data);
 
-    return res.status(201).json({status: "OK"})
-})
+  return res.status(201).json({ status: "OK" });
+});
 
 app.listen(5050, async () => {
-    console.log("Query service started on port 5050")
+  console.log("Query service started on port 5050");
 
-    const res = await axios.get("http://localhost:8080/events")
+  const res = await axios.get("http://localhost:8080/events");
 
-    for (let event of res.data) {
-        console.log("processing event", event.type)
-        handleEvents(event.type, event.data)
-    }
-})
+  for (let event of res.data) {
+    console.log("processing event", event.type);
+    handleEvents(event.type, event.data);
+  }
+});
